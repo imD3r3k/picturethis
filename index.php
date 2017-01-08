6a308f16d62857d71dd1a2fa4f6e5d0dbdffb0ac
Cam Farion <cameronfarion@hotmail.ca>
2017-01-08 10:15:11 -0800

    Created file structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PictureThis</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+  </body>
+  <script type="text/javascript" src="script.js"></script>
+</html>
