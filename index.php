41695589a7a8800f8c29aaec9d77e9e7dec95b83
Cam Farion <cameronfarion@hotmail.ca>
2017-01-13 11:22:33 -0800

    Added include for navbar

--- index.php
+++ index.php
@@ -9,26 +9,10 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-                <a class="navbar-brand" href="#"><img id="logo-icon" src="assets/PT_logo.png" /></a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Prints</a></li>
-                    <li><a href="#">About Us</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
+<?php
+  include("nav.php");
+ ?>
 
 
 </body>
